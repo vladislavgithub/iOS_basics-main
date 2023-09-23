925e6af88a71c90a09248179a4829bf7847a1a18
vladislav <dvora02@mail.ru>
2023-09-23 23:04:17 +0300

    data type tuple

--- ios_basics_elizabet.playground/Contents.swift
+++ ios_basics_elizabet.playground/Contents.swift
@@ -1,3 +1,25 @@
 import UIKit
+/*
+let myProgramming = ( 200, "In work", true)
+myProgramming
+*/
+// Тип данных кортежа
+/*
+ проверка на соответствия типив данных в двух кортежах
+ let tape = (200, "vladik", true)
+ let tape1 = (true, "liza", 200)
+ print(type(of: tape) == type(of: tape1))
+ */
 
-var greeting = "Hello, playground"
+/*
+можно явно определить тип данных кортежа (через двоеточие после имени параметра)
+ let myProgram: (Float, String, Bool) = (200.03, "Hello World", false)
+ print ( myProgram)
+ */
+
+/*
+ // объявляем псевдоним для типа Int
+ typealias ps = Int
+ // объявляем кортеж и инициализируем его значение
+ var korteg: ( Int, Int, ps, ps) = (1,2,2,1)
+ */
